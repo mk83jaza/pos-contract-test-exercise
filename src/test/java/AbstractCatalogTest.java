c94db0a8a9f46d6eca635b87b949e6b189de4805
Markus Kempe <mk83jaza@studserv.uni-leipzig.de>
2024-08-05 13:43:31 +0200

    Make catalog test abstract and create inheritor

--- src/test/java/AbstractCatalogTest.java
+++ src/test/java/AbstractCatalogTest.java
@@ -5,20 +5,24 @@ import java.util.Map;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
-public class CatalogTest {
+public abstract class AbstractCatalogTest {
 	@Test
 	void productFound() {
-		SellOneItemTest.Catalog catalog = new InMemoryCatalog(Map.of(
+		SellOneItemTest.Catalog catalog = createCatalogWith(Map.of(
 				"12345", 895
 		));
 		assertEquals(895, catalog.findPrice("12345"));
 	}
 
+	public abstract InMemoryCatalog createCatalogWith(Map<String, Integer> productMap);
+
 	@Test
 	void productNotFound() {
-		SellOneItemTest.Catalog catalog = new InMemoryCatalog(Map.of(
-				"12345", 895
+		SellOneItemTest.Catalog catalog = createCatalogWithout(Map.of(
+				"99999", 895
 		));
 		assertNull(catalog.findPrice("99999"));
 	}
+
+	public abstract InMemoryCatalog createCatalogWithout(Map<String, Integer> productMap);
 }
