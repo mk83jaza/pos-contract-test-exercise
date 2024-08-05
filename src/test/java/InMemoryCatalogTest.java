a58d847b10e5d48a196bb101d141fbe7554b3e69
Markus Kempe <mk83jaza@studserv.uni-leipzig.de>
2024-08-05 14:32:05 +0200

    Fix merge problem

--- src/test/java/InMemoryCatalogTest.java
+++ src/test/java/InMemoryCatalogTest.java
@@ -2,7 +2,6 @@ import java.util.Collections;
 import java.util.Map;
 
 public class InMemoryCatalogTest extends CatalogContract {
-
 	@Override
 	public SellOneItemTest.Catalog createCatalogWith(Map<String, Integer> productMap) {
 		return new InMemoryCatalog(productMap);
