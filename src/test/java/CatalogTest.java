098ee9d5def81fb6226ddc0403abf4faf786c55f
Markus Kempe <mk83jaza@studserv.uni-leipzig.de>
2024-08-05 14:21:08 +0200

    Prepare making test abstract

--- src/test/java/CatalogTest.java
+++ src/test/java/CatalogTest.java
@@ -1,5 +1,6 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Collections;
 import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,17 +9,25 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 public class CatalogTest {
 	@Test
 	void productFound() {
-		SellOneItemTest.Catalog catalog = new InMemoryCatalog(Map.of(
+		SellOneItemTest.Catalog catalog = createCatalogWith(Map.of(
 				"12345", 895
 		));
 		assertEquals(895, catalog.findPrice("12345"));
 	}
 
+	public SellOneItemTest.Catalog createCatalogWith(Map<String, Integer> productMap) {
+		return new InMemoryCatalog(productMap);
+	}
+
 	@Test
 	void productNotFound() {
-		SellOneItemTest.Catalog catalog = new InMemoryCatalog(Map.of(
-				"12345", 895
+		SellOneItemTest.Catalog catalog = createCatalogWithout(Map.of(
+				"99999", 895
 		));
 		assertNull(catalog.findPrice("99999"));
+	}
+
+	public SellOneItemTest.Catalog createCatalogWithout(Map<String, Integer> productMap) {
+		return new InMemoryCatalog(Collections.emptyMap());
 	}
 }
